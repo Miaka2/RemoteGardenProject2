b24ccd7f232645cacd67683ba5fd12bdb1f4707d
Atticus Blanco <atticusbrujx@gmail.com>
2024-04-17 15:40:12 -0700

    init commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module edu.redwoods.cis18.scam.remotegardenproject2 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens edu.redwoods.cis18.scam.remotegardenproject2 to javafx.fxml;
     exports edu.redwoods.cis18.scam.remotegardenproject2;
 }
